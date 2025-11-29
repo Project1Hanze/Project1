0f93180d58ccc987b08b71b901bf35862543dfd2
Nikgamingnl <nikwarmerdam@gmail.com>
2025-11-29 20:31:29 +0100

    Refactor and clean up code structure

--- Project_1_code_begin.ino
+++ Project_1_code_begin.ino
@@ -1,7 +1,7 @@
 //35 is dood
 //34 is reload
 //2 is shoot
-//4 is revive
+//5 is revive
 
 int reload;
 int vorigeleven  = 1;
@@ -12,7 +12,7 @@ int magazine;
 int magazineuit = 1;
 int trigger;
 int shot;
-/**/
+
 void setup() {
 Serial.begin(9600);
 pinMode(35, INPUT); //tempdoodknop
@@ -21,42 +21,46 @@ pinMode(2, INPUT); //trigger
 pinMode(5, INPUT); //"reset"
 }
 void loop() {
-/*trigger indrukken*/ if (digitalRead(2) == HIGH && shot == 0 && trigger == 0){/*trigger indrukken*/
+/*trigger indrukken*/
+if (digitalRead(2) == HIGH && shot == 0 && trigger == 0){
   trigger = 1;
   shot = 1;
 }
-
-
-
-if (digitalRead(2) == LOW){/*trigger loslaten*/
+/*trigger loslaten*/
+if (digitalRead(2) == LOW){
   trigger = 0;
-  }
-  if (digitalRead(34) == HIGH){/*reload sequence start*/
+}
+/*reload sequence start*/
+if (digitalRead(34) == HIGH){
   reload = 1;
   magazine = 1;
   }else{
     reload = 0;
-  }
-  if (magazine == 1 && magazineuit == 1 && levens == 1){/*reload sequence controlle*/
+}
+/*reload sequence controlle*/
+if (magazine == 1 && magazineuit == 1 && levens == 1){
   ammo = 6;
   magazineuit = 0;
   Serial.println();
   Serial.print(ammo);
   }
-
-  if (ammo == 0){/*tijdelijke out of ammo*/
-    magazine = 0;
-    magazineuit = 1;
+/*tijdelijke out of ammo*/
+if (ammo == 0){
+  magazine = 0;
+   magazineuit = 1;
   }
-if (digitalRead(sensor) == HIGH){/*nu dood maar wordt levens-1 knop*/
+/*nu dood maar wordt levens-1 knop*/
+if (digitalRead(sensor) == HIGH){
   levens = 0;
 }
-if (levens != vorigeleven && levens == 0){/*0 levens? dood*/
+/*0 levens? dood*/
+if (levens != vorigeleven && levens == 0){
   Serial.println();
   Serial.print("dood");
   vorigeleven = levens;
 }
-if(digitalRead(5) == 1 && levens == 0){/*temp reset sequence*/
+/*temp reset sequence*/
+if(digitalRead(5) == 1 && levens == 0){
   levens = 1;
   vorigeleven = levens;
   ammo = 0;
@@ -65,7 +69,8 @@ if(digitalRead(5) == 1 && levens == 0){/*temp reset sequence*/
 
 
 }
-if(shot == 1 && ammo != 0 && levens == 1){/*schiet = -1 ammo met delay*/
+/*schiet = -1 ammo met delay*/
+if(shot == 1 && ammo != 0 && levens == 1){
   ammo = ammo - 1;
   delay(250);
   Serial.println();
@@ -74,10 +79,12 @@ if(shot == 1 && ammo != 0 && levens == 1){/*schiet = -1 ammo met delay*/
 if(shot == 1){
   shot = 0;
 }
-if (reload == 1 && ammo != 0 && ammo != 6 && levens == 1){/*nog kogels = trage reload*/
+/*nog kogels = trage reload*/
+if (reload == 1 && ammo != 0 && ammo != 6 && levens == 1){
   delay(1500); 
   magazineuit = 1;
 }
 }
+
 
 
